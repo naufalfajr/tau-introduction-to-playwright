e09af0f3328ce64fca9e283cb3d5ad621b0f9907
naufalfajr <muhnaufalfajri@gmail.com>
2025-11-13 08:19:32 +0700

    chapter 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,27 +5,24 @@ import baseEnvUrl from './utils/environmentBaseUrl';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require('dotenv').config();
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   // testDir: './tests',
-
   /* Run tests in files in parallel */
   fullyParallel: true,
-
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
   retries: 2,
-
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   // reporter: [['html', { open: 'always' }]], //always, never and on-failure (default).
@@ -41,11 +38,10 @@ export default defineConfig({
   /**
    * custom reports: https://playwright.dev/docs/test-reporters#custom-reporters 
   */
-  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,10 +67,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        // viewport: { width: 1280, height: 720 },
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     {
@@ -148,16 +141,16 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,24 +2,44 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
   await expect(page).toHaveURL(/.*intro/);
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('check Java page', async ({ page }) => {
+test.only('check Java Page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.getByRole('link', { name: 'Get started' }).click();
+  
   await page.getByRole('button', { name: 'Node.js' }).hover();
-  await page.getByText('Java', { exact: true }).click();
-  // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
-  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
-  await expect(page.getByText('Installing Playwright', { exact: true })).not.toBeVisible();
-  const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
-  await expect(page.getByText(javaDescription)).toBeVisible();
+  // await page.locator('.dropdown__menu').waitFor({ state: 'visible' });
+  await expect(page.locator('.dropdown__menu')).toBeVisible();
+  await page.getByRole('link', { name: 'Java', exact: true }).click();
+  await expect(page).toHaveURL(/java/);
+
+  await expect(page.getByText("Installing Playwright", { exact: true })).not.toBeVisible();
+
+  const javaDesc = "Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.";
+  await expect(page.getByText(javaDesc, { exact: true })).toBeVisible();
 });
 
+/** 
+ * 1. open page playwright.dev
+ * 2. click get started link
+ * 3. mouse hover  the language dropdown
+ * 4. click at java
+ * 5. check the URL contains java
+ * 6. check the text "Installing Playwright" in not displayed
+ * 7. check the text is displayed "Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation."
+ */
